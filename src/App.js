62fdded0bf36ccce55c471ac106b8d5031f4b75e
zzs <zzs>
2019-05-25 01:07:20 +0800

    components : add header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,9 +20,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+        <DatePicker />
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
