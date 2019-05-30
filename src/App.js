078391277bd9bffed0745f562ddae4207d806533
zzs <zzs>
2019-05-30 20:21:53 +0800

    use class instead of function

--- src/App.js
+++ src/App.js
@@ -5,27 +5,29 @@ import { DatePicker } from 'antd';
 import 'antd/dist/antd.css';
 import Header from './components/Header.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <div className="AppBody">
-        <img src={logo} className="Applogo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="Applink"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <DatePicker />
-          Learn React
-        </a>
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <div className="AppBody">
+          <img src={logo} className="Applogo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="Applink"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <DatePicker />
+            Learn React
+          </a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
+  
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './Header.scss';
 import { Tag } from 'antd'
 
-function Header() {
-  return (
-    <div className="AppHeader">
-      <div className="AppName">
-        <span className="AppNameText">React-Test</span>
-        <Tag className="AppTag">I'm a tag</Tag>
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="AppHeader">
+        <div className="AppName">
+          <span className="AppNameText">React-Test</span>
+          <Tag className="AppTag">I'm a tag</Tag>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default Header;
