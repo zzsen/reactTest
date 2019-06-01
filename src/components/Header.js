93ab85b80a10a4b5e586668e7955ffe615914945
zzs <296099296@qq.com>
2019-06-01 17:40:02 +0800

    edit the loading method of app menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,69 +1,151 @@
 import React from 'react';
 import './Header.scss';
+import { HashRouter as Router, NavLink } from "react-router-dom"
 import { Tag, Menu, Icon } from 'antd'
 const { SubMenu }  = Menu;
 
 class Header extends React.Component {
   state = {
     current: 'mail',
-  };
+    menus: [ 
+      {
+        name: 'one',
+        path: '/one',
+        exact: true
+      }, {
+        name: 'two',
+        path: '/two',
+        exact: true,
+        children: [ 
+          {
+            name: 'group-one',
+            menus: [ 
+              {
+              name: 'two-one',
+              path: '/two_one',
+              exact: true
+              }, 
+              {
+              name: 'two-two',
+              path: '/two_two',
+              exact: true
+              } 
+            ]
+          },
+          {
+            name: 'group-two',
+            menus: [ 
+                {
+                  name: 'two-one',
+                  path: '/two_one',
+                  exact: true
+                }, {
+                  name: 'two-two',
+                  path: '/two_two',
+                  exact: true
+                }
+              ]
+          }
+        ]
+      }, {
+        name: 'three',
+        path: '/three',
+        exact: true
+      } 
+    ]
+  }
 
   handleClick = e => {
     console.log('click ', e);
     this.setState({
       current: e.key,
+      ...this.state
     });
-  };
+  }
 
-  render() {
-    return (
-      <div className="AppHeader">
-        <div className="AppName">
-          <span className="AppNameText">React-Test</span>
-          <Tag className="AppTag">I'm a tag</Tag>
-        </div>
-
-        
-        <div className="logout">
-          用户名称
-          <Icon type="logout" />
-        </div>
+  /**
+   * menu渲染
+   *
+   * @returns
+   * @memberof Header
+   */
+  getMenus () {
+    const menus = this.state.menus.map((menuItem)=>
+      menuItem.children ? (
+        <SubMenu
+          key={menuItem.path}
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="setting" />
+              {menuItem.name}
+            </span>
+          }
+        >
+        {
+          menuItem.children.map(sonGroup=>
+            <Menu.ItemGroup title={sonGroup.name}>
+            {
+              sonGroup.menus.map(subMenu=>
+                <Menu.Item key={subMenu.path}>
+                  <NavLink
+                    to={subMenu.path}
+                    exact={subMenu.exact}
+                    activeClassName="navActive">
+                    {subMenu.name}
+                  </NavLink>
+                </Menu.Item>
+              )
+            }
+            </Menu.ItemGroup>
+          )
+        }
+        </SubMenu>
+      ) : (
+        <Menu.Item 
+          key={menuItem.name}>
+          <NavLink
+            to="/One"
+            exact={menuItem.exact}
+            activeClassName="navActive">
+            <Icon type="mail" />
+            {menuItem.name}
+          </NavLink>
+        </Menu.Item>
+      )
+    )
+    return ( 
+      <Router>
         <Menu
           className="AppMenu"
           onClick={this.handleClick}
           selectedKeys={[this.state.current]}
           mode="horizontal">
-          <Menu.Item key="mail">
-            <Icon type="mail" />
-            Navigation One
-          </Menu.Item>
-          <Menu.Item key="app" disabled>
-            <Icon type="appstore" />
-            Navigation Two
-          </Menu.Item>
-          <SubMenu
-            title={
-              <span className="submenu-title-wrapper">
-                <Icon type="setting" />
-                Navigation Three - Submenu
-              </span>
-            }
-          >
-            <Menu.ItemGroup title="Item 1">
-              <Menu.Item key="setting:1">Option 1</Menu.Item>
-              <Menu.Item key="setting:2">Option 2</Menu.Item>
-            </Menu.ItemGroup>
-            <Menu.ItemGroup title="Item 2">
-              <Menu.Item key="setting:3">Option 3</Menu.Item>
-              <Menu.Item key="setting:4">Option 4</Menu.Item>
-            </Menu.ItemGroup>
-          </SubMenu>
-          <Menu.Item key="alipay">
-            <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-              Navigation Four - Link
-            </a>
-          </Menu.Item>
+          {menus}
         </Menu>
+      </Router> )
+  }
+
+  render() {
+    return (
+      <div className="AppHeader">
+        <Router>
+          <NavLink
+            to="/"
+            exact={true}
+            activeClassName="navActive">
+            <div className="AppName">
+              <span className="AppNameText">React-Test</span>
+              <Tag className="AppTag">I'm a tag</Tag>
+            </div>
+          </NavLink>
+        </Router>
+        <div className="AppUserName">
+          <span className="userInfo">用户名称</span>
+          <span className="logoutBtn">
+            <Icon type="logout" />退出
+          </span>
+        </div>
+        {this.getMenus()}
       </div>
     )
   }
